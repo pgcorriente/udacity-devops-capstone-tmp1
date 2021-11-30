2df9a81c96bb92c5f6b6c281775e2c45916058cc
pgcorriente <46505826+pgcorriente@users.noreply.github.com>
2021-11-30 16:14:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world v2"
+    return "Hello world 2 - Electric Boogaloo"
 
 if __name__ == "__main__":
     # This NEEDS to run on port 0.0.0.0, otherwise the port can't be exposed outside the container
